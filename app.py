e8bb2ff1e147c8e4ccc75f812adde27fd19bcd22
ElijahKotary <elijah.a.kotary@gmail.com>
2022-01-24 22:41:54 -0500

    Added Month table

--- app.py
+++ app.py
@@ -4,10 +4,26 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://ftbkschqwtljwb:afce27db674440599b3e0c8ed2c4aa956c3351f0b5691dac3626d0dd681ace85@ec2-3-216-113-109.compute-1.amazonaws.com:5432/dc28mb408i45v8"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+class Month(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    year = db.Column(db.String, nullable=False)
+    days_in_month = db.Column(db.Integer, nullable=False)
+    days_in_previous_month = db.Column(db.Integer, nullable=False)
+    start_day = db.Integer(db.Integer, nullable=False)
+
+    def __init__(self, name, year, days_in_month, days_in_previous_month, start_day):
+        self.name = name
+        self.year = year
+        self.days_in_month = days_in_month
+        self.days_in_previous_month = days_in_previous_month
+        self.start_day = start_day
+
 
 
 
